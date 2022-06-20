a00243d1a6d48c9d9aa27f5be547c796669017ad
Daniele Conti <fourlastor@users.noreply.github.com>
2022-06-20 09:01:55 +0100

    Move input processing to system

--- core/src/main/kotlin/io/github/fourlastor/jamjam/level/LevelScreen.kt
+++ core/src/main/kotlin/io/github/fourlastor/jamjam/level/LevelScreen.kt
@@ -1,7 +1,6 @@
 package io.github.fourlastor.jamjam.level
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.utils.viewport.FitViewport
@@ -9,6 +8,7 @@ import com.github.quillraven.fleks.World
 import io.github.fourlastor.jamjam.JamGame
 import io.github.fourlastor.jamjam.level.system.BodiesListener
 import io.github.fourlastor.jamjam.level.system.Box2dComponent
+import io.github.fourlastor.jamjam.level.system.InputSystem
 import io.github.fourlastor.jamjam.level.system.LayerComponent
 import io.github.fourlastor.jamjam.level.system.PhysicsDebugSystem
 import io.github.fourlastor.jamjam.level.system.PhysicsSystem
@@ -41,8 +41,10 @@ class LevelScreen(
         World {
             inject<Camera>(viewport.camera)
             inject(box2dWorld)
+            inject(game)
             componentListener<BodiesListener>()
             inject(PhysicsSystem.Config(step = 1f / 60f))
+            system<InputSystem>()
             system<PhysicsSystem>()
             system<RenderSystem>()
             system<PhysicsDebugSystem>()
@@ -55,6 +57,10 @@ class LevelScreen(
             }
 
     override fun show() {
+        Gdx.input.inputProcessor = world.system<InputSystem>().inputProcessor
+    }
+
+    override fun hide() {
         Gdx.input.inputProcessor = null
     }
 
@@ -63,23 +69,8 @@ class LevelScreen(
     }
 
     override fun render(delta: Float) {
-        handleInput()
         camera.update()
         world.update(delta)
-    }
-
-    private val factor = 1f / 10f
-
-    private fun handleInput() {
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            camera.translate(-factor, 0f, 0f)
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            camera.translate(factor, 0f, 0f)
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
-            game.goToMenu()
-        }
     }
 
     override fun dispose() {
