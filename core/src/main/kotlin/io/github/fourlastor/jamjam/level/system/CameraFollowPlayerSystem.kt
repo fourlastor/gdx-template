0718e36bd2f63d37bad62c689c8390e5d4ea532b
Daniele Conti <fourlastor@users.noreply.github.com>
2022-06-21 19:32:55 +0100

    Reduce camera updates

--- core/src/main/kotlin/io/github/fourlastor/jamjam/level/system/CameraFollowPlayerSystem.kt
+++ core/src/main/kotlin/io/github/fourlastor/jamjam/level/system/CameraFollowPlayerSystem.kt
@@ -12,8 +12,15 @@ class CameraFollowPlayerSystem(
     private val camera: Camera,
     private val bodies: ComponentMapper<DynamicBodyComponent>,
 ) : IteratingSystem() {
+
+    private var lastX = camera.position.x
     override fun onTickEntity(entity: Entity) {
         val center = bodies[entity].body.position
-        camera.update { position.x = center.x }
+        val x = center.x
+        if (lastX == x) {
+            return
+        }
+        lastX = x
+        camera.update { position.x = x }
     }
 }
